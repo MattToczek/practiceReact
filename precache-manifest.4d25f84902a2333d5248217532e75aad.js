3d8437c52427565481eaa816560f59ad2d2fa425
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-21 11:00:17 +0000

    Updates

---
+++ precache-manifest.4d25f84902a2333d5248217532e75aad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3ca289b4f3ef8802b9151655bc95098",
+    "url": "/practiceReact/index.html"
+  },
+  {
+    "revision": "fa57a64aa75a7371f54b",
+    "url": "/practiceReact/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "43b685c54d6ec04ec1e7",
+    "url": "/practiceReact/static/js/2.79b00d78.chunk.js"
+  },
+  {
+    "revision": "fa57a64aa75a7371f54b",
+    "url": "/practiceReact/static/js/main.f03c2e09.chunk.js"
+  },
+  {
+    "revision": "bc60ca051b874ed6aef2",
+    "url": "/practiceReact/static/js/runtime-main.7f9be9b0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/practiceReact/static/media/logo.25bf045c.svg"
+  }
+]);
